4c117cbd6573ef89fe11aa9de6767e56c96af5ee
Nehaa1210 <121329987+Nehaa1210@users.noreply.github.com>
2023-08-25 11:12:22 +0530

    Add files via upload

---
+++ src/Components/AllRoutes.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./Home";
+import Login from "./Login";
+
+function AllRoutes() {
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+      </Routes>
+    </div>
+  );
+}
+
+export default AllRoutes;
